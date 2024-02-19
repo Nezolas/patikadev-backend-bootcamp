1db2904d6a2d9a18ed522f816105e977b704d60c
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-20 01:33:38 +0300

    Not Ortalama Hesaplamasi

--- Week 1/not-ortalamasi-hesaplayan-program/src/Main.java
+++ Week 1/not-ortalamasi-hesaplayan-program/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /*
+        kodlari tekrara dusurmemek icin ders isimleri bir string arrayinin icerisinde tutuluyor,
+        karsilik gelecek notlar ise classes arrayinin uzunlugu referans alinarak tutuluyor.
+        NOT: Program, kullanici girdilerinin hepsinin 0-100 arasi olacagi dusunulerek tasarlanmistir.
+        */
 
+        String[] classes = {"Matematik", "Fizik", "Kimya", "Turkce", "Tarih", "Muzik"};
+        int[] notes = new int[classes.length];
+        int sum = 0;
+        double avarage;
+        // Kullanici ders notlarini girebilmesi icin bir input olusturuluyor
+        Scanner input = new Scanner(System.in);
+        // for dongusu yardimiyla ders notlari sira ile notes arrayinin icerisine kaydediliyor
+        for( int i = 0; i < classes.length; i++){
+            System.out.print("Notunuzu giriniz"+ " (" + classes[i] + "):\t");
+            notes[i] = Integer.parseInt(input.nextLine());
+        }
+        // Bir baska for dongusu ile notlar toplaniyor
+        for( int j = 0; j < classes.length; j++){
+            sum += notes[j];
+        }
+        // toplam, array icerisine yazilan derslerin sayisina bolunerek ortalama aliniyor.
+        avarage = sum / classes.length;
+        System.out.println("Derslerinizin ortalamasi: " + avarage);
+        var pass = (60 < avarage) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.print("***** " + pass + " *****");
     }
 }
