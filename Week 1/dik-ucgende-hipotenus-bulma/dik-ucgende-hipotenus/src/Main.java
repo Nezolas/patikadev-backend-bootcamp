19e6005de721189596b75b357c72ad3d798cbf15
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-21 00:56:29 +0300

    hipotenus-hesabi-yapan-basit-program

--- Week 1/dik-ucgende-hipotenus-bulma/dik-ucgende-hipotenus/src/Main.java
+++ Week 1/dik-ucgende-hipotenus-bulma/dik-ucgende-hipotenus/src/Main.java
@@ -3,14 +3,19 @@ import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         int a, b;
-        double c;
+        double c, u, perimeter, area;
         Scanner input = new Scanner(System.in);
         System.out.print("Ilk kenarin degeri: ");
         a = input.nextInt();
         System.out.print("Ikinci kenarin degeri: ");
         b = input.nextInt();
-
         c = Math.sqrt((a * a) + (b * b));
-        System.out.print("Hipotenus degeri: " + c);
+        System.out.println("Hipotenus degeri: " + c);
+        perimeter = a + b + c;
+        u = perimeter / 2;
+        area = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.print("Ucgenin alani: " + area);
+
+
     }
 }
