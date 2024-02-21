03deb704a4bfb9987faac2eab0e66bbb4db949f6
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-21 23:39:33 +0300

    islem-onceligi-pratik-calismasi

--- Week 1/aritmetik-islem-onceligi/src/Main.java
+++ Week 1/aritmetik-islem-onceligi/src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // kullanıcan istenecek değişkenler tanımlanıyor.
         int a, b, c, calc;
         System.out.println("a+b*c-b işleminin yapılması için:");
+        // Scanner sınıfı ile kullanıcıdan veriler isteniyor
         Scanner number = new Scanner(System.in);
         System.out.print("Birinci sayıyı girin: ");
         a = number.nextInt();
@@ -11,7 +13,9 @@ public class Main {
         b = number.nextInt();
         System.out.print("Üçüncü sayıyı girin: ");
         c = number.nextInt();
+        // işlem sırasına göre hesaplamayapılıyor.
         calc = a + b * c - b;
+        // yapılan işlemin sonucu ekrana yazdırılıyor.
         System.out.print("İşlem Sonucu: " + calc);
     }
 }
