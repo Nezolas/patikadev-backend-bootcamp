cb553d065f6331fff84480f7ea839a012da1bf7a
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-22 00:16:20 +0300

    islem-onceligi-pratik-calismasi

--- Week 1/manav-kasa/manav-kasa/src/Main.java
+++ Week 1/manav-kasa/manav-kasa/src/Main.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] gn = {"Armut", "Elma", "Domates", "Muz", "Patlıcan"}; //grocery name holding in this array
-        double[] gp = {2.14, 3.67, 1.11, 0.95, 5.00}; //grocery prices list holding in this array with respect to gn array order
-        double[] gw = new double[gp.length] ; //grocery weights list is going to hold in this array
+        String[] gn = {"Armut", "Elma", "Domates", "Muz", "Patlıcan"}; //Grocery name holding in this array
+        double[] gp = {2.14, 3.67, 1.11, 0.95, 5.00}; //Grocery prices list holding in this array with respect to gn array order
+        double[] gw = new double[gp.length] ; //Grocery weights list is going to hold in this array
         double total = 0;
         Scanner price = new Scanner(System.in);
+        //The weight values are being taken from the user.
         for( int i = 0; i < gp.length; i++){
             System.out.print(gn[i] + " Kaç kilo? ");
             gw[i] = price.nextDouble();
         }
-
+        //The weight values are multiplied by the price per kilogram and added up total.
         for( int j = 0; j < gp.length; j++){
             total += gp[j] * gw[j];
         }
