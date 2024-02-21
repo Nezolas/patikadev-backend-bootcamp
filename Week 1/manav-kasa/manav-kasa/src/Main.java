a3dee149d22d32795ef29adc9a806ed576a6c708
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-21 22:51:14 +0300

    manav-kasa-programi

--- Week 1/manav-kasa/manav-kasa/src/Main.java
+++ Week 1/manav-kasa/manav-kasa/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] gn = {"Armut", "Elma", "Domates", "Muz", "Patlıcan"};
-        double[] gp = {2.14, 3.67, 1.11, 0.95, 5.00}; //grossery prices list holding in an array with respect to gn order
-        double[] gw = new double[gp.length] ; //grossery weights list is going to hold in an array
+        String[] gn = {"Armut", "Elma", "Domates", "Muz", "Patlıcan"}; //grocery name holding in this array
+        double[] gp = {2.14, 3.67, 1.11, 0.95, 5.00}; //grocery prices list holding in an array with respect to gn order
+        double[] gw = new double[gp.length] ; //grocery weights list is going to hold in an array
         double total = 0;
         Scanner price = new Scanner(System.in);
         for( int i = 0; i < gp.length; i++){
