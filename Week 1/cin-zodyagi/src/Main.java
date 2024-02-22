5d7bca7be1206225483f34d53ef117b8829d2f5b
Nezolas <insbaglanti@gmail.com>
2024-02-22 10:16:43 +0300

    cin-zodyagi-bulan-program

---
+++ Week 1/cin-zodyagi/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int bYear, residual;
+        System.out.println("***************************\n" + "\tÇin Zodyağı Bulma\n" + "***************************\n" );
+        System.out.print("Doğum Yılınızı Girıniz: ");
+        Scanner input = new Scanner(System.in);
+        bYear = input.nextInt();
+        residual = bYear % 12;
+        if(bYear == (int)bYear){
+            System.out.print("Çin Zodyağı Burcunuz : ");
+            switch (residual) {
+                case 0:
+                    System.out.println("Maymun");
+                    break;
+                case 1:
+                    System.out.println("Horoz");
+                    break;
+                case 2:
+                    System.out.println("Köpek");
+                    break;
+                case 3:
+                    System.out.println("Domuz");
+                    break;
+                case 4:
+                    System.out.println("Fare");
+                    break;
+                case 5:
+                    System.out.println("Öküz");
+                    break;
+                case 6:
+                    System.out.println("Kaplan");
+                    break;
+                case 7:
+                    System.out.println("Tavşan");
+                    break;
+                case 8:
+                    System.out.println("Ejderha");
+                    break;
+                case 9:
+                    System.out.println("Yılan");
+                    break;
+                case 10:
+                    System.out.println("At");
+                    break;
+                case 11:
+                    System.out.println("Koyun");
+                    break;
+            }
+
+
+        }
+
+    }
+}
