2fdb19242800eced452148e05a6e5a0ab2236207
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-02-23 02:33:43 +0300

    kullanici-bilgileri-sorgulaniyor

--- Week 1/kullanici-girisi/src/Main.java
+++ Week 1/kullanici-girisi/src/Main.java
@@ -9,22 +9,27 @@ public class Main {
 
         System.out.print("Sifreniz: ");
         pw = inp.nextLine();
-
+        //password check
         if(uname.equals("patika") && pw.equals("java123")){
             System.out.print("Giris Yaptiniz!");
         }
         else{
+        //regenerating password
             System.out.println("Bilgileriniz Yanlis!");
             System.out.print("Sifreyi Sifirlamak ister misin (Y/N)? ");
             request = inp.nextLine();
             if( request.equals("Y")){
-               System.out.print("Yeni sifrenizi girin: ");
-                npw = inp.nextLine();
-                if(npw.equals("java123")){
-                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+        //if users choose old password program looping until users input a different password
+                do {
+                    System.out.print("Yeni sifrenizi girin: ");
+                    npw = inp.nextLine();
+        //This if statement is used to prevent unnecessary printing of error message.
+                    if(npw.equals("java123")) {
+                        System.out.println("Yeni şifre eski şifre ile aynı olamaz. Lütfen başka bir şifre deneyin.");
+                    }
                 }
-                else
-                    System.out.println("Sifre Olusturuldu.");
+                while (npw.equals("java123"));
+                System.out.println("Sifre olusturuldu.");
             }
             else if(request.equals("N")){
                 System.out.println("Cikis yapiliyor.");
@@ -32,6 +37,5 @@ public class Main {
             else
                 System.out.println("Gecersiz istek.");
         }
-
     }
 }
