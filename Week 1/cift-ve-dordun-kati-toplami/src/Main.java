5f38f5ef42f26f860a74cf0482eece705a2cabf2
nezolas <insbaglanti@gmail.com>
2024-02-24 23:23:25 +0300

    girilen sayilardan-dordun-katlari-toplami

---
+++ Week 1/cift-ve-dordun-kati-toplami/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, sum = 0;
+        Scanner inp = new Scanner(System.in);
+        do {
+            System.out.println("Sayı Giriniz!");
+            n = inp.nextInt();
+            if(n % 4 == 0){
+                sum += n;
+            }
+        }
+        while(n % 2 == 0);
+        System.out.println("Toplam:" + sum );
+    }
+}
