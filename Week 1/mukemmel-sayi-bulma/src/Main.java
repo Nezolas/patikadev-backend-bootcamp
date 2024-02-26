6ef453c0c632dc54f4d0a6436530630f72664112
Nezolas <insbaglanti@gmail.com>
2024-02-26 11:10:28 +0300

    mukemell-sayi

---
+++ Week 1/mukemmel-sayi-bulma/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        int toplam = 0;
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
+            }
+        }
+
+        if (toplam == sayi) {
+            System.out.println(sayi + " mükemmel sayıdır.");
+        } else {
+            System.out.println(sayi + " mükemmel sayı değildir.");
+        }
+    }
+}
