a11af64cd9533027889d6b2db2923802334841fb
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-03-04 01:32:46 +0300

    palindromik-2

---
+++ Week-2/palindromik/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+    }
+}
