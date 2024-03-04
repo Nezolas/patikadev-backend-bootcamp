724ae3a2cb70ae39365d463dffba109a548e1ba0
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-03-05 00:32:14 +0300

    sort-array

---
+++ Week-2/eleman-siralama/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String args[])
+    {
+        Main ob = new Main();
+        int arr[] = {64,25,12,22,11};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
