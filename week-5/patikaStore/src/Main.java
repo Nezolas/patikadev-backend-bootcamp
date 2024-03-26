ebb76f033858c4b36abbc3b3050178f23f812b54
Nezolas <insbaglanti@gmail.com>
2024-03-26 11:15:35 +0300

    patika-store-main-restore

--- week-5/patikaStore/src/Main.java
+++ week-5/patikaStore/src/Main.java
@@ -8,34 +8,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true){
-            System.out.println("1. Ürün Ekle");
-            System.out.println("2. Ürünleri Listele");
-            System.out.println("3. Ürün Sil");
-            System.out.println("4. Marka Listele");
-            System.out.println("5. Çıkış");
+            System.out.println("1. Cep Telefonu İşlemleri");
+            System.out.println("2. Notebook İşlemleri");
+            //System.out.println("1. Ürün Ekle");
+            //System.out.println("2. Ürünleri Listele");
+            //System.out.println("3. Ürün Sil");
+            System.out.println("3. Ürünleri Listele");
+            System.out.println("0. Çıkış");
             System.out.print("Seçiminiz: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice){
                 case  1:
-                    System.out.println("Cep Telofonu Eklemek için 1' e basın..");
-                    System.out.println("Notebook  Eklemek için 2' ye basın..");
-                    int selection = scanner.nextInt();
-                    System.out.println("Ürün Adı: ");
-                    String brandName = scanner.nextLine();
-                    System.out.print("Birim Fiyat: ");
-                    double unitPrice = scanner.nextDouble();
-                    System.out.print("Stok Miktarı: ");
-                    int stockAmount = scanner.nextInt();
-                    System.out.print("Brand ID: ");
-                    int brandID = scanner.nextInt();
+                    System.out.println("1. Ürün Ekle");
+                    System.out.println("2. Ürün Sil");
+                    System.out.print("Seçiminiz: ");
+                    int operation = scanner.nextInt();
+                    if(operation == 1) {
+                        int selection = 1;
+                        System.out.print("Ürün Adı: ");
+                        String brandName = scanner.next();
+                        System.out.print("Birim Fiyat: ");
+                        double unitPrice = scanner.nextDouble();
+                        System.out.print("Stok Miktarı: ");
+                        int stockAmount = scanner.nextInt();
+                        System.out.print("Brand ID: ");
+                        int brandID = scanner.nextInt();
+                        scanner.nextLine();
+                        Brand brand = store.getBrandById(brandID);
+                        if (brand != null) {
+                            int storage, camera, ram;
+                            double screenSize, battery;
 
-                    scanner.nextLine();
-                    Brand brand = store.getBrandById(brandID);
-                    if (brand != null){
-                        int storage, camera, ram;
-                        double screenSize, battery;
-                        if (selection == 1){
                             System.out.print("Depolama (GB): ");
                             storage = scanner.nextInt();
                             System.out.print("Ekran Boyutu (inç): ");
@@ -45,12 +49,43 @@ public class Main {
                             System.out.print("Pil Gücü (mAh): ");
                             battery = scanner.nextDouble();
                             scanner.nextLine();
-                            Product cepTelefonu = new Product(unitPrice,stockAmount,brandName, brand,storage,screenSize,camera,battery);
-                            store.addProduct(cepTelefonu,selection);
-                        }else {
+                            Product cepTelefonu = new Product(unitPrice, stockAmount, brandName, brand, storage, screenSize, camera, battery);
+                            store.addProduct(cepTelefonu, selection);
+                            System.out.println("Product Eklendi");
+                        } else {
+                            System.out.println("Geçersiz Marka ID");
+                        }
+                        break;
+                    }
+                    if(operation == 2){
+                        System.out.println("Silmek istediğiniz ürünü seçin");
+                        int deleetedID = scanner.nextInt();
+                        store.deleteProduct(deleetedID);
+                        System.out.println("Ürün Silindi");
+                        break;
+                    }
+                case  2:
+                    System.out.println("1. Ürün Ekle");
+                    System.out.println("2. Ürün Sil");
+                    System.out.print("Seçiminiz: ");
+                    operation = scanner.nextInt();
+                    if(operation == 1) {
+                        int selection = 2;
+                        System.out.print("Ürün Adı: ");
+                        String brandName = scanner.next();
+                        System.out.print("Birim Fiyat: ");
+                        double unitPrice = scanner.nextDouble();
+                        System.out.print("Stok Miktarı: ");
+                        int stockAmount = scanner.nextInt();
+                        System.out.print("Brand ID: ");
+                        int brandID = scanner.nextInt();
+                        scanner.nextLine();
+                        Brand brand = store.getBrandById(brandID);
+                        if (brand != null){
+                            int storage, camera, ram;
+                            double screenSize, battery;
                             System.out.print("Depolama (GB): ");
                             storage = scanner.nextInt();
-
                             System.out.print("RAM (GB): ");
                             ram = scanner.nextInt();
                             System.out.print("Ekran Boyutu (inç): ");
@@ -58,25 +93,27 @@ public class Main {
                             scanner.nextLine();
                             Product notebook = new Product(unitPrice,stockAmount,brandName, brand,storage,screenSize,ram);
                             store.addProduct(notebook,selection);
+                            System.out.println("Product Eklendi");
+                        }else {
+                            System.out.println("Geçersiz brand ID");
                         }
-                        System.out.println("Product Eklendi");
-                    }else {
-                        System.out.println("Geçersiz brand ID");
+                        break;
                     }
-                    break;
-                case 2:
-                    store.listProduct();
-                    break;
+                    if(operation == 2){
+                        System.out.println("Silmek istediğiniz ürünü seçin");
+                        int deleetedID = scanner.nextInt();
+                        store.deleteProduct(deleetedID);
+                        System.out.println("Ürün Silindi");
+                        break;
+                    }
+
                 case 3:
-                    System.out.println("Silmek istediğiniz ürünü seçin");
-                    int silinecekId = scanner.nextInt();
-                    store.deleteProduct(silinecekId);
-                    System.out.println("Product Silindi");
+                    store.listProduct();
                     break;
                 case 4:
                     store.listBrand();
                     break;
-                case 5:
+                case 0:
                     System.out.println("Programdan çıkılıyor...");
                     System.exit(0);
                 default:
@@ -84,6 +121,4 @@ public class Main {
             }
         }
     }
-
-
 }
