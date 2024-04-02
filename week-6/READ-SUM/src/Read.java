8440008f9449173b00368f3d782b352c10b24ea1
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-04-02 10:24:50 +0300

    Read-from-file-and-sum

---
+++ week-6/READ-SUM/src/Read.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Read {
+    public static void main(String[] args) {
+
+
+        try {
+            FileReader path = new FileReader("C:\\Users\\...\\num_file.txt"); //Please copy your file path here
+            BufferedReader input = new BufferedReader(path);
+            String line;
+            int sum = 0;
+
+            while ((line = input.readLine()) != null) {
+                int num = Integer.parseInt(line);
+                sum+= num;
+            }
+
+            System.out.println("Toplam: " + sum);
+        } catch (IOException e) {
+            System.err.println("Dosya okuma hatası: " + e.getMessage());
+        }
+    }
+}
