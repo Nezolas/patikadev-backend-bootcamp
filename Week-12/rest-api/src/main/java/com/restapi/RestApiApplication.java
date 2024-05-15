38dda02bb3b7468f85eb810580344a3f6159f740
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-05-15 14:22:16 +0300

    readME

--- Week-12/rest-api/src/main/java/com/restapi/RestApiApplication.java
+++ Week-12/rest-api/src/main/java/com/restapi/RestApiApplication.java
@@ -1,12 +1,9 @@
 package com.restapi;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title="Rest API Practice",version = "1.0",description = "Library Application with Rest API"))
 public class RestApiApplication {
 
     public static void main(String[] args) {
